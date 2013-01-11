d120b501ae6d2c888e7529ee6beb97cccff2a30b
Brent Millare <brent.millare@gmail.com>
2013-01-11 17:13:45 -0500

    scratch

--- src/dj/math/install.clj
+++ src/dj/math/install.clj
@@ -10,13 +10,12 @@
       "dj/view/cljs")
 (def e (dj.view/generator dj.view/emit))
 (def re (dj.view/generator dj.view/raw-emit))
-(dj.dependencies/resolve-project "gnuplot")
-(load "gnuplot")
-(def fe gnuplot/emit-static-plot)
 (def de (dj.view.cljs/generator app (dj.view.cljs/feed-out
                                      dj.view/emit)))
 (def dre (dj.view.cljs/generator app (dj.view.cljs/feed-out
                                       dj.view/raw-emit)))
+(def dhe (dj.view.cljs/generator app (dj.view.cljs/feed-out
+                                      identity)))
 (def dfe (dj.view.cljs/generator app (dj.view.cljs/feed-out
                                       dj.view/iframe-emit)))
 
@@ -29,18 +28,92 @@
       "dj/math/differentiation"
       "dj/math/linearalgebra")
 
+(load "dj/cuda/install")
+(dj.cuda.install/load-jcuda
+ {:architecture :64
+  :version "5.0"
+  :os :linux})
+
 (require '[datomic.api :as d])
-(dre (let [s (seq (d/q '[:find ?ret
-                         :where
-                         [?e :pos :k-check]
-                         [?e :v ?ret]]
-                       @store))]
-       [s (count s)]))
+
+(dj.dependencies/resolve-project "dj.plot")
+(load "dj/cuda"
+      "dj/plot")
 
 (de (dj.git/changed-projects))
-(def v (atom []))
 
 (dj.repl/deftracer t (dj.repl/->trace-walker (dj.repl/->tuple-trace-logger 'user/v)
                                              2))
+(defn get-val [code d]
+  (-> (d/q
+       '[:find ?v
+         :in $ ?code
+         :where
+         [?id :result ?v]
+         [?id :code ?c]
+         [(= ?c ?code)]]
+       d
+       code)
+      vec
+      first
+      first))
 
+(def v (atom []))
+@v
 
+(let [settings {:num-nodes 1
+                :dt 1.0
+                :record-dt 40.0
+                :end-time 4000.0
+                :model-file (dj.io/file "dj/cuda/models/stiff.model")
+
+                ;; cuda specific
+                :kernel-name "mitomodel"
+                :cuda-device-id 0
+                :ptxas-path "/usr/local/cuda/bin/ptxas"
+                :nvcc-path "/usr/local/cuda/bin/nvcc"
+         
+                :integrator
+                #_ dj.cuda.translator/forward-euler-integrator-fn
+                (dj.cuda.translator/->rosenbrock-wrap-integrator-fn dj.cuda.translator/backward-formulation)}
+      r (dj.cuda.translator/full-model-config settings)
+      initial-conditions (mapv (fn [n_idx]
+                                 (reduce (fn [m [s v]]
+                                           (assoc m
+                                             (keyword s)
+                                             (Double/parseDouble (dj/substring v
+                                                                               0
+                                                                               -1))))
+                                         {}
+                                         (:initial-conditions-assignment r)))
+                               (range (:num-nodes r)))
+      r #_ (let [r (dj.cuda.translator/->emit-lisp-solver r)
+              r (eval (r initial-conditions))
+              r (merge settings
+                       {:records r})
+              r (dj.cuda.translator/lisp-plot-format r)]
+          (first r))
+      (-> r
+          dj.cuda/compile-run
+          dj.cuda/plot-format
+          first)]
+  (let [
+        r (select-keys r ["x" "z"])
+        r (reduce (fn [m k]
+                    (-> m
+                        (assoc-in [k :width]
+                                  3)
+                        (assoc-in [k :series-label]
+                                  (str k))))
+                  r
+                  (keys r))
+        r (dj.plot/multiplot {:title (dj.io/get-path (:model-file settings))
+                              :x-label "time"
+                              :y-label "z(t)"}
+                             (vals r))
+        r (dj.plot/chart->image r 640 480)
+        r (dj.plot/image->png-binary r)]
+    (dhe {:tag :img
+          :attrs {:src (dj.view/inline-binary-url r "image/png")}}))
+  
+  )
